62034f37d8b1c252167a4cd8b9917be70176a89a
hitanshu0729 <146374587+hitanshu0729@users.noreply.github.com>
2024-06-09 17:59:19 +0530

    Update conn.mjs

--- src/db/conn.mjs
+++ src/db/conn.mjs
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+require("dotenv").config();
 mongoose
-  .connect("mongodb://localhost:27017/webt")
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connection Successful");
   })
